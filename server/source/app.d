ee2c6b1ed4c7100e1a81bdf7512cc33ce6a4b55b
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-09-12 03:32:34 -0600

    Add data/items.json

--- server/source/app.d
+++ server/source/app.d
@@ -25,13 +25,11 @@ void main()
 	/* 
 	 * `Server` loop runs on main thread
 	 * `Server` instantiation results in the creation and subsequent running of `World` thread.
-	 * `Server` loop receives incoming messages, deserializes them into `Packet` objects and uses 
-	   d's messaging protocol to pass them to the `World` thread.
+	 * `Server` loop receives incoming messages, deserializes them into `Packet` objects and uses d's messaging protocol to pass them to the `World` thread.
 	 * The `World` thread then processes those messages and invokes the corresponding logic for each.
 	 * ....profit?
 	*/
 	auto server = new Server();
 	server.loadWorld();
-	auto serverTid = server.id;
 	server.start();
 }
